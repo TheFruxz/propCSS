cd2c7a966e55fb8679a6ec92b19df9ba3f692887
TheFruxz <cedricspitzer@outlook.de>
2024-03-06 20:49:05 +0100

    feature: provide dedicated CSSProperties type

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,11 +1,16 @@
 // Reexport your entry components here
 
 /**
+ * This type is used to define css properties, where the key is the css property name (like backgroundColor) and the value is the css property value
+ */
+export type CSSProperties = { [key: string]: any }
+
+/**
  * This function (used inside a svelte use:propCSS) is used to set multiple css properties on an element
  * @param element the (from svelte) provided element, to which the css properties will be applied
  * @param properties an object with css properties, where the key is the css property name (like backgroundColor) and the value is the css property value
  */
-export default function propCSS(element: HTMLElement, properties: { [key: string]: any }) {
+export default function propCSS(element: HTMLElement, properties: CSSProperties) {
     for (const key in properties) {
       const kebabKey = key.replace(/([A-Z])/g, '-$1').toLowerCase() // replace camelCase with kebab-case
   
