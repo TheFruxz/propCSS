37441ef29fccb9d55cc3ee643550f77bbfe134ab
TheFruxz <cedricspitzer@outlook.de>
2024-03-06 19:27:22 +0100

    chore: add documentation to propCSS function

--- src/lib/PropCSS.ts
+++ src/lib/PropCSS.ts
@@ -1,3 +1,8 @@
+/**
+ * This function (used inside a svelte use:propCSS) is used to set multiple css properties on an element
+ * @param element the (from svelte) provided element, to which the css properties will be applied
+ * @param properties an object with css properties, where the key is the css property name (like backgroundColor) and the value is the css property value
+ */
 export default function propCSS(element: HTMLElement, properties: { [key: string]: any }) {
   for (const key in properties) {
     const kebabKey = key.replace(/([A-Z])/g, '-$1').toLowerCase() // replace camelCase with kebab-case
